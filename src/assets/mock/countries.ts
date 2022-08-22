11f14503cf99bcc90807300ea64b5843d6fdcf17
Rinor Zekaj <zekajrinor1@gmail.com>
2022-08-22 06:48:09 +0200

    feat: adds mock file with country list of europe

---
+++ src/assets/mock/countries.ts
@@ -0,0 +1,58 @@
+const countries = {
+  europe: [
+    {
+      name: "Albania",
+      flag: "🇦🇱",
+    },
+    {
+      name: "Belgium",
+      flag: "🇧🇪",
+    },
+    {
+      name: "Czechia",
+      flag: "🇨🇿",
+    },
+    {
+      name: "France",
+      flag: "🇫🇷",
+    },
+    {
+      name: "Germany",
+      flag: "🇩🇪",
+    },
+    {
+      name: "Italy",
+      flag: "🇮🇹",
+    },
+    {
+      name: "Kosovo",
+      flag: "🇽🇰",
+    },
+    {
+      name: "Netherlands",
+      flag: "🇳🇱",
+    },
+    {
+      name: "Poland",
+      flag: "🇵🇱",
+    },
+    {
+      name: "Romania",
+      flag: "🇷🇴",
+    },
+    {
+      name: "Russia",
+      flag: "🇷🇺",
+    },
+    {
+      name: "Ukraine",
+      flag: "🇺🇦",
+    },
+    {
+      name: "United Kingdom",
+      flag: "🇩🇬",
+    },
+  ],
+};
+
+export default countries;
