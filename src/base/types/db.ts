cd8ff43bb8f2640fa5c4dc7006769dedab4910eb
Rinor Zekaj <zekajrinor1@gmail.com>
2022-10-09 17:46:42 +0200

    feat: adds member type, adds check if member

--- src/base/types/db.ts
+++ src/base/types/db.ts
@@ -1,8 +1,15 @@
 export type Community = {
-  id: number;
+  id: string;
   name: string;
   created_at: string;
   type: string;
   creator_id: string;
   country: string;
+};
+
+export type Member = {
+  community_id: string;
+  member_id: string;
+  created_at: string;
+  approved: boolean;
 };
--- src/pages/app/[country]/[community]/index.tsx
+++ src/pages/app/[country]/[community]/index.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import moment from "moment";
 import {
   getUser,
+  supabaseClient,
   supabaseServerClient,
   withPageAuth,
 } from "@supabase/auth-helpers-nextjs";
-import { Community as CommunityType } from "@/base/types/db";
+import { Community as CommunityType, Member } from "@/base/types/db";
 import AppLayout from "@/components/layouts/app-layout";
 
-export default function Community({ community }: { community: CommunityType }) {
+export default function Community({
+  community,
+  member,
+}: {
+  community: CommunityType;
+}) {
+  console.log("member", member);
+
+  const handleJoinCommunity = async () => {
+    const { data, status } = await supabaseClient
+      .from<Member>("community_members")
+      .insert({}, { returning: "minimal" });
+    console.log("TEST");
+  };
+
   return (
     <AppLayout title={`${community.name} - Community`}>
       <h4>Community: {community.name}</h4>
       <p>Created at: {moment(community.created_at).format("DD MMM YYYY")}</p>
       <p>Created by: {community.creator_id}</p>
+      <button onClick={handleJoinCommunity}>Join Community</button>
     </AppLayout>
   );
 }
@@ -33,8 +49,17 @@ export const getServerSideProps = withPageAuth({
         .eq("name", communityName)
         .eq("country", country);
 
+      const { body: member } = await supabaseServerClient(ctx)
+        .from<Member>("community_members")
+        .select("*")
+        .eq("community_id", community[0].id)
+        .eq("member_id", user.id);
+
+      console.log("community[0].id", community[0].id);
+      console.log("user.id", user.id);
+
       return {
-        props: { ...props, community: community[0] },
+        props: { ...props, community: community[0], member: member[0] },
       };
     }
 
