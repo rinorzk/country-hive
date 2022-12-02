ce56ee0829feda24c4a6e298e8d4f03237f1717b
Rinor Zekaj <zekajrinor1@gmail.com>
2022-12-02 09:12:04 +0100

    fix: imporves jsx in message component

--- src/components/sections/room-chat/message/message.tsx
+++ src/components/sections/room-chat/message/message.tsx
@@ -10,14 +10,17 @@ export default function Message(props: MessageProps) {
   }
 
   return (
-    <li className={styles.message}>
-      <div>
-        {message.replyOf ? (
-          <small>&#8594; {message.replyOf?.content}</small>
-        ) : null}
-      </div>
-      <div style={{ display: "flex" }}>
-        {message?.content} <span onClick={handleReplyTo}>reply</span>
+    <li className={styles.messageItem}>
+      {message.replyOf ? (
+        <small className={styles.messageReply}>
+          &#8594; {message.replyOf?.content}
+        </small>
+      ) : null}
+      <div className={styles.messageContent}>
+        <p>{message.content}</p>
+        <span className={styles.replyBtn} onClick={handleReplyTo}>
+          reply
+        </span>
       </div>
     </li>
   );
