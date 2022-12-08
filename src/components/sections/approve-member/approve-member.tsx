ee12dfff66089f75b0a7babc4558e7f5e3d6ef87
Rinor Zekaj <zekajrinor1@gmail.com>
2022-12-08 08:08:16 +0100

    feat: uses members and lists them in apporve member component

--- src/components/sections/approve-member/approve-member.tsx
+++ src/components/sections/approve-member/approve-member.tsx
@@ -1,8 +1,19 @@
+import { useCommunityMembers } from "@/base/hooks/useCommunityMembers";
+import { Member } from "@/base/types/db";
 import React, { useState } from "react";
 import { ApproveMemberProps } from "./types";
 
-export default function ApproveMembers({ onSubmit }: ApproveMemberProps) {
+export default function ApproveMembers({
+  onSubmit,
+  communityId,
+  userId,
+}: ApproveMemberProps) {
   const [username, setUsername] = useState("");
+  const { members } = useCommunityMembers({ communityId, userId });
+
+  function renderMember(member: Member) {
+    return <li>{member.member?.username}</li>;
+  }
 
   function handleApproveMember(e: React.FormEvent<Element>) {
     e.preventDefault();
@@ -15,11 +26,13 @@ export default function ApproveMembers({ onSubmit }: ApproveMemberProps) {
         <input
           type="text"
           name="approve-user"
+          placeholder="add new user"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
         <button type="submit">Add user</button>
       </form>
+      <ul>{members.length ? members.map(renderMember) : null}</ul>
     </div>
   );
 }
