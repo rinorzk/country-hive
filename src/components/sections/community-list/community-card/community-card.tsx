1c3f702567479ce64cc31da64f306cecf8327d93
Rinor Zekaj <zekajrinor1@gmail.com>
2023-01-21 11:56:17 +0100

    fix: fixes image for community card

--- src/components/sections/community-list/community-card/community-card.tsx
+++ src/components/sections/community-list/community-card/community-card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { Community } from "@/base/types/db";
 import styles from "./community-card.module.scss";
 import CommunityCover from "@/components/elements/community-cover";
@@ -12,10 +13,12 @@ export default function CommunityCard({
   return (
     <div className={styles.card}>
       <div className={styles.coverHolder}>
-        {cover_url ? <CommunityCover src={cover_url || ""} alt={name} /> : null}
+        {cover_url ? <CommunityCover src={cover_url} alt={name} /> : null}
       </div>
       <div className={styles.avatarHolder}>
-        <img src={avatar_url} />
+        {avatar_url ? (
+          <Image src={avatar_url} alt={name} width={70} height={70} />
+        ) : null}
       </div>
       <h4 className={styles.name}>{name}</h4>
       <p className={styles.description}>{description}</p>
